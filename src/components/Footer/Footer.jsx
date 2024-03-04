eed6b2ca0d7c39c793617c42f43c6a958ac74289
Chaweewan211 <65020823@up.ac.th>
2024-03-04 15:14:21 +0700

    finish

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyleft @ 2567. All right mate.</p>
+    </footer>
+  );
+}
+
+export default Footer;
